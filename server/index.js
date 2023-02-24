35a29a7acd92a010b87509d2294ef0189370adfc
Alin Ibadof <alinibadof@gmail.com>
2023-02-24 15:35:54 +0200

    add configurations for the imported middleware, and file storage

--- server/index.js
+++ server/index.js
@@ -8,3 +8,30 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
+// midleware configurations
+const __filename = fileURLToPath(import.meta.url); // for grabbing file URL
+const __dirname = path.dirname(__filename); // getting the directory name
+dotenv.config(); // so dotenv can be used
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors()); // invoke the CORS policies
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); //sets the directory to where we keep the assets (which in this case is locally)
+
+// configuring file storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
